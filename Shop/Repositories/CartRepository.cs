b7dbf3859aecc97b6c561980904ddf382dced3c4
Dedside0 <georguzitaev@gmail.com>
2025-10-17 20:44:57 +0300

    added clear cart button

--- Shop/Repositories/CartRepository.cs
+++ Shop/Repositories/CartRepository.cs
@@ -54,4 +54,34 @@ public static class CartRepository
             }
         }
     }
+
+    public static void Remove(Product? product, string userId)
+    {
+        var existingCart = TryGetById(userId);
+
+        if (existingCart != null)
+        {
+            var existingCartItem = existingCart.Items.FirstOrDefault(x => x.Product.Id == product.Id);
+            if (existingCartItem.Count == 1)
+            {
+                DeleteCartItem(existingCart,existingCartItem);
+            }
+            else
+            {
+                existingCartItem.Count--;
+            }
+        }
+    }
+
+    public static void DeleteCartItem(Cart cart,CartItem cartItem)
+    {
+        cart.Items.Remove(cartItem);
+    }
+
+    public static void Clear(string id)
+    {
+        var existingCart = TryGetById(id);
+        if(existingCart != null) 
+            _carts.Remove(existingCart);
+    }
 }
