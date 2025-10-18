485e35313446afd566931e97f43de77bf7cf0ebe
Dedside0 <georguzitaev@gmail.com>
2025-10-18 19:01:48 +0300

    order view

--- Shop/obj/Debug/net8.0/Shop.AssemblyInfo.cs
+++ Shop/obj/Debug/net8.0/Shop.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebdd23dba707d9ac27e3d2c79e4e05df3d00fbf9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5f0093be6d217516c432c3a2e1bd64da75a6cf3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
